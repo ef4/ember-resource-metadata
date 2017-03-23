087af6b4e3184a8753506e37e7295e125a11e9e9
Edward Faulkner <ef@alum.mit.edu>
2017-03-23 16:01:48 -0400

    use an integration test for our helper instead of a unit test
    because it's stateful

---
+++ tests/integration/helpers/meta-for-resource-test.js
@@ -0,0 +1,35 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+import DS from 'ember-data';
+import Ember from 'ember';
+
+moduleForComponent('meta-for-resource', 'Integration | Helper | meta for resource', {
+  integration: true,
+  beforeEach() {
+    this.register('model:example', DS.Model.extend({
+      title: DS.attr('string')
+    }));
+    this.inject.service('ember-resource-metadata', { as: 'metadata' });
+    this.inject.service('store');
+    this.set('model', this.get('store').createRecord('example', { title: 'hello' }));
+  }
+});
+
+test('it works when meta already exists', function(assert) {
+  this.get('metadata').write(this.get('model'), { someMetaKey: 42 });
+  this.render(hbs`<div class="output">{{get (meta-for-resource model) 'someMetaKey'}}</div>`);
+  assert.equal(this.$('.output').text(), '42');
+});
+
+test('it works when meta is set later', function(assert) {
+  this.render(hbs`<div class="output">{{get (meta-for-resource model) 'someMetaKey'}}</div>`);
+
+  assert.equal(this.$('.output').text(), '');
+
+  Ember.run(() => {
+    this.get('metadata').write(this.get('model'), { someMetaKey: 42 });
+  });
+
+  assert.equal(this.$('.output').text(), '42');
+
+});
