ac25e5dad3d80eb1776345943d069bb86f28f633
Scott Newcomer <snewcomer24@gmail.com>
2020-09-06 15:49:20 -0700

    3.16

--- tests/integration/adapters/adapter-test.js
+++ tests/integration/adapters/adapter-test.js
@@ -1,4 +1,6 @@
-import { moduleForComponent, test } from 'ember-qunit';
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import '@ember/test-helpers';
 import DS from 'ember-data';
 import RSVP from 'rsvp';
 import AdapterMixin from 'ember-resource-metadata/adapter-mixin';
@@ -6,177 +8,178 @@ import AdapterMixin from 'ember-resource-metadata/adapter-mixin';
 let answers;
 let requests;
 
-moduleForComponent('adapter', 'Integration | Adapter | adapter', {
-  integration: true,
-  beforeEach() {
+module('Integration | Adapter | adapter', function(hooks) {
+  setupRenderingTest(hooks);
+
+  hooks.beforeEach(function() {
     answers = [];
     requests = [];
-    this.register('model:example', DS.Model.extend({
+    this.owner.register('model:example', DS.Model.extend({
       title: DS.attr('string'),
       references: DS.hasMany('references', { async: false })
     }));
-    this.register('model:reference', DS.Model.extend({
+    this.owner.register('model:reference', DS.Model.extend({
       source: DS.attr('string')
     }));
-    this.register('adapter:example', DS.JSONAPIAdapter.extend(AdapterMixin, {
+    this.owner.register('adapter:example', DS.JSONAPIAdapter.extend(AdapterMixin, {
       ajax(url, type, options) {
         requests.push({ url, type, options });
         return RSVP.resolve(answers.shift());
       }
     }));
-    this.inject.service('resource-metadata', { as: 'metadata' });
-    this.inject.service('store');
-  }
-});
+    this.metadata = this.owner.lookup('service:resource-metadata');
+    this.store = this.owner.lookup('service:store');
+  });
 
-test('it sets meta when loading a record for the first time', function(assert) {
-  answers.push({
-    data: {
-      type: 'examples',
-      id: 1,
-      meta: {
-        something: 42
-      }
-    }
-  });
-  return RSVP.resolve().then(() => {
-    return this.get('store').findRecord('example', 1);
-  }).then(record => {
-    assert.equal(this.get('metadata').read(record).get('something'), 42);
-  });
-});
-
-test('it passes options through to super findRecord', function(assert) {
-  answers.push({
-    data: {
-      type: 'examples',
-      id: 1,
-      meta: {
-        something: 42
-      }
-    }
-  });
-  return RSVP.resolve().then(() => {
-    return this.get('store').findRecord('example', 1, { include: 'other' });
-  }).then(() => {
-    assert.deepEqual(requests[0].options, { data: { include: 'other'} });
-  });
-});
-
-
-test('it sets meta when loading a record for the first time via queryRecord', function(assert) {
-  answers.push({
-    data: {
-      type: 'examples',
-      id: 1,
-      meta: {
-        something: 42
-      }
-    }
-  });
-  return RSVP.resolve().then(() => {
-    return this.get('store').queryRecord('example', {});
-  }).then(record => {
-    assert.equal(this.get('metadata').read(record).get('something'), 42);
-  });
-});
-
-test('it sets meta when loading a record for the first time via query', function(assert) {
-  answers.push({
-    data: [{
-      type: 'examples',
-      id: 1,
-      meta: {
-        something: 42
-      }
-    }]
-  });
-  return RSVP.resolve().then(() => {
-    return this.get('store').query('example', {});
-  }).then(records => {
-    assert.equal(this.get('metadata').read(records.get('firstObject')).get('something'), 42);
-  });
-});
-
-
-test('it updates meta when a record is updated', function(assert) {
-  answers.push({
-    data: {
-      type: 'examples',
-      id: 1,
-      meta: {
-        something: 42
-      }
-    }
-  });
-  answers.push({
-    data: {
-      type: 'examples',
-      id: 1,
-      meta: {
-        something: 43
-      }
-    }
-  });
-  return RSVP.resolve().then(() => {
-    return this.get('store').findRecord('example', 1);
-  }).then(record => {
-    return record.save();
-  }).then(record => {
-    assert.equal(this.get('metadata').read(record).get('something'), 43);
-  });
-});
-
-test('it updates meta when a record is created', function(assert) {
-  answers.push({
-    data: {
-      type: 'examples',
-      id: 1,
-      meta: {
-        something: 43
-      }
-    }
-  });
-  return RSVP.resolve().then(() => {
-    return this.get('store').createRecord('example');
-  }).then(record => {
-    return record.save();
-  }).then(record => {
-    assert.equal(this.get('metadata').read(record).get('something'), 43);
-  });
-});
-
-test('it sets meta in included records', function(assert) {
-  answers.push({
-    data: {
-      type: 'examples',
-      id: 1,
-      meta: {
-        something: 42
-      }
-    },
-    included: [
-      {
+  test('it sets meta when loading a record for the first time', function(assert) {
+    answers.push({
+      data: {
+        type: 'examples',
         id: 1,
-        type: 'references',
         meta: {
-          something: 24
+          something: 42
         }
-      },
-      {
-        id: 2,
-        type: 'references',
-        meta: {
-          something: 20
-        }
-      },
-    ]
+      }
+    });
+    return RSVP.resolve().then(() => {
+      return this.get('store').findRecord('example', 1);
+    }).then(record => {
+      assert.equal(this.get('metadata').read(record).get('something'), 42);
+    });
   });
-  return RSVP.resolve().then(() => {
-    return this.get('store').findRecord('example', 1);
-  }).then(record => {
-    assert.equal(this.get('metadata').read(record).get('something'), 42);
-    assert.equal(this.get('metadata').read({ id: 1, type: 'reference' }).get('something'), 24);
-    assert.equal(this.get('metadata').read({ id: 2, type: 'reference' }).get('something'), 20);
+
+  test('it passes options through to super findRecord', function(assert) {
+    answers.push({
+      data: {
+        type: 'examples',
+        id: 1,
+        meta: {
+          something: 42
+        }
+      }
+    });
+    return RSVP.resolve().then(() => {
+      return this.get('store').findRecord('example', 1, { include: 'other' });
+    }).then(() => {
+      assert.deepEqual(requests[0].options, { data: { include: 'other'} });
+    });
+  });
+
+
+  test('it sets meta when loading a record for the first time via queryRecord', function(assert) {
+    answers.push({
+      data: {
+        type: 'examples',
+        id: 1,
+        meta: {
+          something: 42
+        }
+      }
+    });
+    return RSVP.resolve().then(() => {
+      return this.get('store').queryRecord('example', {});
+    }).then(record => {
+      assert.equal(this.get('metadata').read(record).get('something'), 42);
+    });
+  });
+
+  test('it sets meta when loading a record for the first time via query', function(assert) {
+    answers.push({
+      data: [{
+        type: 'examples',
+        id: 1,
+        meta: {
+          something: 42
+        }
+      }]
+    });
+    return RSVP.resolve().then(() => {
+      return this.get('store').query('example', {});
+    }).then(records => {
+      assert.equal(this.get('metadata').read(records.get('firstObject')).get('something'), 42);
+    });
+  });
+
+
+  test('it updates meta when a record is updated', function(assert) {
+    answers.push({
+      data: {
+        type: 'examples',
+        id: 1,
+        meta: {
+          something: 42
+        }
+      }
+    });
+    answers.push({
+      data: {
+        type: 'examples',
+        id: 1,
+        meta: {
+          something: 43
+        }
+      }
+    });
+    return RSVP.resolve().then(() => {
+      return this.get('store').findRecord('example', 1);
+    }).then(record => {
+      return record.save();
+    }).then(record => {
+      assert.equal(this.get('metadata').read(record).get('something'), 43);
+    });
+  });
+
+  test('it updates meta when a record is created', function(assert) {
+    answers.push({
+      data: {
+        type: 'examples',
+        id: 1,
+        meta: {
+          something: 43
+        }
+      }
+    });
+    return RSVP.resolve().then(() => {
+      return this.get('store').createRecord('example');
+    }).then(record => {
+      return record.save();
+    }).then(record => {
+      assert.equal(this.get('metadata').read(record).get('something'), 43);
+    });
+  });
+
+  test('it sets meta in included records', function(assert) {
+    answers.push({
+      data: {
+        type: 'examples',
+        id: 1,
+        meta: {
+          something: 42
+        }
+      },
+      included: [
+        {
+          id: 1,
+          type: 'references',
+          meta: {
+            something: 24
+          }
+        },
+        {
+          id: 2,
+          type: 'references',
+          meta: {
+            something: 20
+          }
+        },
+      ]
+    });
+    return RSVP.resolve().then(() => {
+      return this.get('store').findRecord('example', 1);
+    }).then(record => {
+      assert.equal(this.get('metadata').read(record).get('something'), 42);
+      assert.equal(this.get('metadata').read({ id: 1, type: 'reference' }).get('something'), 24);
+      assert.equal(this.get('metadata').read({ id: 2, type: 'reference' }).get('something'), 20);
+    });
   });
 });
--- tests/integration/helpers/meta-for-resource-test.js
+++ tests/integration/helpers/meta-for-resource-test.js
@@ -1,35 +1,37 @@
 import { run } from '@ember/runloop';
-import { moduleForComponent, test } from 'ember-qunit';
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { find, render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 import DS from 'ember-data';
 
-moduleForComponent('meta-for-resource', 'Integration | Helper | meta for resource', {
-  integration: true,
-  beforeEach() {
-    this.register('model:example', DS.Model.extend({
+module('Integration | Helper | meta for resource', function(hooks) {
+  setupRenderingTest(hooks);
+
+  hooks.beforeEach(function() {
+    this.owner.register('model:example', DS.Model.extend({
       title: DS.attr('string')
     }));
-    this.inject.service('resource-metadata', { as: 'metadata' });
-    this.inject.service('store');
+    this.metadata = this.owner.lookup('service:resource-metadata');
+    this.store = this.owner.lookup('service:store');
     this.set('model', this.get('store').createRecord('example', { title: 'hello' }));
-  }
-});
-
-test('it works when meta already exists', function(assert) {
-  this.get('metadata').write(this.get('model'), { someMetaKey: 42 });
-  this.render(hbs`<div class="output">{{get (meta-for-resource model) 'someMetaKey'}}</div>`);
-  assert.equal(this.$('.output').text(), '42');
-});
-
-test('it works when meta is set later', function(assert) {
-  this.render(hbs`<div class="output">{{get (meta-for-resource model) 'someMetaKey'}}</div>`);
-
-  assert.equal(this.$('.output').text(), '');
-
-  run(() => {
-    this.get('metadata').write(this.get('model'), { someMetaKey: 42 });
   });
 
-  assert.equal(this.$('.output').text(), '42');
+  test('it works when meta already exists', async function(assert) {
+    this.get('metadata').write(this.get('model'), { someMetaKey: 42 });
+    await render(hbs`<div class="output">{{get (meta-for-resource model) 'someMetaKey'}}</div>`);
+    assert.equal(find('.output').text(), '42');
+  });
 
+  test('it works when meta is set later', async function(assert) {
+    await render(hbs`<div class="output">{{get (meta-for-resource model) 'someMetaKey'}}</div>`);
+
+    assert.equal(find('.output').text(), '');
+
+    run(() => {
+      this.get('metadata').write(this.get('model'), { someMetaKey: 42 });
+    });
+
+    assert.equal(find('.output').text(), '42');
+  });
 });
